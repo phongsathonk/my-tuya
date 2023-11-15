86884e5fbfaae9cc51fb89fb4a7e723d732647ef
phongsathonk <80814680+phongsathonk@users.noreply.github.com>
2023-11-15 14:46:34 +0700

    Update requestUtil.js

--- utils/requestUtil.js
+++ utils/requestUtil.js
@@ -5,8 +5,8 @@ module.exports.get = (options) => {
   return new Promise((resolve, reject) => {
     request
       .get(options.url)
-      .set('Authorization', `Bearer ${options.accessToken}`)
-      // .set('Content-Type', 'application/json')
+      // .set('Authorization', `Bearer ${options.accessToken}`)
+      .set('Content-Type', 'application/x-www-form-urlencoded')
       .end((err, res) => {
         if (err) { return reject(err) }
         return resolve(res.body)
@@ -19,8 +19,9 @@ module.exports.post = (options) => {
     request
       .post(options.url)
       .send(options.body)
-      .set('Authorization', `Bearer ${options.accessToken}`)
+      // .set('Authorization', `Bearer ${options.accessToken}`)
       // .set('Accept', 'application/json')
+      .set('Accept', 'application/x-www-form-urlencoded')      
       .end((err, res) => {
         if (err) { return reject(err) }
         return resolve(res.body)
